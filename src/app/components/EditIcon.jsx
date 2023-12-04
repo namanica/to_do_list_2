6edb6630794cce4814587e02273d1498d8d0d420
namanica <veronichkapilipna@gmail.com>
2023-12-04 13:08:40 +0200

    imported icons

---
+++ src/app/components/EditIcon.jsx
@@ -0,0 +1,19 @@
+export const Editicon = (props) => (
+  <svg
+  width="1em"
+  height="1em"
+  viewBox="0 0 24 24"
+  xmlns="http://www.w3.org/2000/svg"
+  {...props}
+  >
+<path
+fill="none"
+troke="currentColor"
+strokeLinecap="round"
+strokeLinejoin="round"
+strokeWidth="2"
+d="M12 20h9M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1l1-4L16.5 3.5z"
+>
+</path>
+</svg>
+)
